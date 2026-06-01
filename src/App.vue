3c76c01d8e38e1ab747605fa5b3215c9c66d3702
Avon46 <142923631+Avon46@users.noreply.github.com>
2026-06-01 17:29:03 +0800

    login add

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const username = ref('')
+const password = ref('')
+function handleLogin() {
+  console.log('帳號：', username.value)
+  console.log('密碼：', password.value)
+}
+</script>
+
+<template>
+  <h1>登入</h1>
+  <p>帳號</p>
+  <input v-model="username" placeholder="請輸入帳號" />
+  <p>密碼</p>
+  <input v-model="password" type="password" placeholder="請輸入密碼" />
+  <button @click="handleLogin">登入</button>
+</template>
+
+<style scoped></style>
